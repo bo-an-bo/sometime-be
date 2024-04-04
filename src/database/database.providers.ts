fa6ad1e17793ac32925ebecdb232e1bcca190652
w8385 <w8385@naver.com>
2024-04-04 23:54:01 +0900

    refactor: DatabaseProvider

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
+import { ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
     provide: 'MONGODB_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.MONGODB_URI),
+    useFactory: (): Promise<typeof mongoose> => {
+      const configService = new ConfigService();
+      const uri = configService.get('MONGODB_URI');
+      return mongoose.connect(uri);
+    },
   },
 ];
