d6344665c6178b5f68329032123c5403a965a89f
w8385 <w8385@naver.com>
2024-04-05 01:43:48 +0900

    fix: SERVER_PORT > .env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configSwagger } from './config.swagger';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configSwagger(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 
-bootstrap().then();
+bootstrap().then(() => {
+  console.log(
+    `Server running on http://localhost:${process.env.SERVER_PORT}/api`,
+  );
+});
