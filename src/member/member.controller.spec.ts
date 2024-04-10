94b58a8a9df81e7010059e5a2c37fe6ea1398d7d
ssoxong <ssoxong@naver.com>
2024-04-10 21:50:52 +0900

    fix: member crud npm test

--- src/member/member.controller.spec.ts
+++ src/member/member.controller.spec.ts
@@ -1,7 +1,7 @@
 import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
 import { Member } from './interfaces/member.interface';
-import { Model } from 'mongoose';
+import { HydratedDocument, Model } from 'mongoose';
 
 describe('MemberController', () => {
   let memberController: MemberController;
@@ -13,7 +13,15 @@ describe('MemberController', () => {
     memberController = new MemberController(memberService);
   });
 
-  it('should be defined', () => {
-    expect(memberController).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of members', async () => {
+      const result: HydratedDocument<any> = [
+        { _id: 1, name: '테스트1' },
+        { _id: 2, name: '테스트2' },
+      ];
+      jest.spyOn(memberService, 'findAll').mockImplementation(() => result);
+
+      expect(await memberController.findAll()).toBe(result);
+    });
   });
 });
--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -1,18 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { MemberService } from './member.service';
+import { Member } from './interfaces/member.interface';
+import { HydratedDocument, Model } from 'mongoose';
 
 describe('MemberService', () => {
-  let service: MemberService;
+  let memberService: MemberService;
+  let memberModel: Model<Member>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MemberService],
-    }).compile();
-
-    service = module.get<MemberService>(MemberService);
+    memberService = new MemberService(memberModel);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of members', async () => {
+      const result: HydratedDocument<any> = [
+        { _id: 1, name: '테스트1' },
+        { _id: 2, name: '테스트2' },
+      ];
+      jest.spyOn(memberService, 'findAll').mockImplementation(() => result);
+
+      expect(await memberService.findAll()).toBe(result);
+    });
   });
 });
