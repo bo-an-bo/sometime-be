b6439608ba31778d29155736f118801b70330336
w8385 <w8385@naver.com>
2024-04-11 05:16:31 +0900

    refactor: DB 이름 넣어주면서 살짝 변경

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,8 +6,12 @@ export const databaseProviders = [
     provide: 'MONGODB_CONNECTION',
     useFactory: (): Promise<typeof mongoose> => {
       const configService = new ConfigService();
-      const uri = configService.get('MONGODB_URI');
-      return mongoose.connect(uri);
+      const uri = configService.get<string>('MONGODB_URI');
+      const dbName = configService.get<string>('MONGODB_DB_NAME');
+
+      return mongoose.connect(uri, {
+        dbName,
+      });
     },
   },
 ];
