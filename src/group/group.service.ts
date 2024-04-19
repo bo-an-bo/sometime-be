32fbaa14eab529345a0f2b497d277f39fe5eedbc
ssoxong <ssoxong@naver.com>
2024-04-20 02:08:26 +0900

    fix: ae87376 array일때 실행

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -46,12 +46,12 @@ export class GroupService {
 
   async deleteMembers(groupId: string, memberIds: string[]): Promise<void> {
     const group = (await this.groupRepository.findOne(groupId)) as Group;
-    if (!Array.isArray(memberIds)) memberIds = [memberIds];
 
-    group.members = group.members.filter(
-      (member) => !memberIds.includes(member),
-    );
-
+    if (Array.isArray(memberIds)) {
+      group.members = group.members.filter(
+        (member) => !memberIds.includes(member),
+      );
+    }
     await this.groupRepository.update(groupId, group);
   }
 
