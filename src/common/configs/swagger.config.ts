a39636c1bfb011a0ee2dfc386117479d36f8d9c5
박근형 <me@w8385.dev>
2024-05-01 06:52:18 +0900

    feat: /auth swagger 반영

--- src/common/configs/swagger.config.ts
+++ src/common/configs/swagger.config.ts
@@ -1,9 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as process from 'node:process';
 
+import { AuthModule } from '../../auth/auth.module';
 import { EventModule } from '../../event/event.module';
 import { GroupModule } from '../../group/group.module';
 import { MemberModule } from '../../member/member.module';
+import { TransactionModule } from '../../transaction/transaction.module';
 import { UserModule } from '../../user/user.module';
 
 export const swaggerConfig = (app: INestApplication<any>) => {
@@ -12,12 +15,11 @@ export const swaggerConfig = (app: INestApplication<any>) => {
     .addTag('Group', '모임 관련 API')
     .addTag('Member', '모임 회원 관련 API')
     .addTag('Event', '이벤트 관련 API')
-    .addTag('Auth', 'OAuth 인증 관련 API')
-    .addTag('User', '사용자 관련 API')
+    .addTag('Transaction', '거래 내역 관련 API')
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
-    include: [GroupModule, MemberModule, EventModule, UserModule],
+    include: [GroupModule, MemberModule, EventModule, TransactionModule],
   });
   SwaggerModule.setup('api', app, document);
 };
@@ -29,10 +31,33 @@ export const devSwaggerConfig = (app: INestApplication<any>) => {
     .addTag('Group', '모임 관련 API')
     .addTag('Member', '모임 회원 관련 API')
     .addTag('Event', '이벤트 관련 API')
-    .addTag('Auth', 'OAuth 인증 관련 API')
-    .addTag('User', '사용자 관련 API')
+    .addTag('Transaction', '거래 내역 관련 API')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('dev', app, document);
+};
+
+export const authSwaggerConfig = (app: INestApplication<any>) => {
+  const authorizationUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
+  const config = new DocumentBuilder()
+    .setTitle('sometime API')
+    .addTag('Auth', 'OAuth 인증 관련 API')
+    .addTag('User', '사용자 관련 API')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        authorizationCode: {
+          authorizationUrl,
+          tokenUrl: 'https://kauth.kakao.com/oauth/token',
+          scopes: undefined,
+        },
+      },
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [AuthModule, UserModule],
+  });
+  SwaggerModule.setup('auth', app, document);
 };
